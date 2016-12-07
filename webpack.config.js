eb12498e89fcda66f1c3807c4ee4e18764ca8814
mat <reallymathew@gmail.com>
2016-12-06 22:43:15 -0600

    oops, forgot to remove unnecessary comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,3 @@
-// var path = require('path');
-
-// var webpack = require('webpack');
-
-// var packageData = require('./package.json');
-
-// var filename = [packageData.name, packageData.version, 'js'];
-
-// module.exports = {
-//     entry: path.resolve(__dirname, packageData.main),
-//     output: {
-//         path: path.resolve(__dirname, 'build/dev/public'),
-//         filename: filename.join('.'),
-//     },
-//     devtool: 'source-map',
-//     module: {
-//         loaders: [
-//             {
-//                 test: /\.js$/,
-//                 exclude: /(node_modules)/,
-//                 loader: 'babel'
-//             }
-//         ]
-//     }
-// };
-
-
 var path = require('path');
 
 var webpack = require('webpack');
