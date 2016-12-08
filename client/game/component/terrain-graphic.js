c188f53ee2d5facdbab040f368fd534a520bc21e
JR <jovincent.ruiz@gmail.com>
2016-12-08 10:12:38 -0800

    maybe map works? idk FoN

--- client/game/component/terrain-graphic.js
+++ client/game/component/terrain-graphic.js
@@ -1,1 +1,28 @@
 //this is where I load the grass and dirt
+export default function makeLevelGraphicComponent(spec) {
+	console.log('makin the land.. like god')
+
+	var entity = spec.entityOfComponent;
+	var dirt = new Image()
+	dirt.src = spec.dirt;
+	var grass = new Image()
+	grass.src = spec.grass;
+
+	function drawMap(ctx,map) {
+		for(var y = 0;y<(map.dimensions);y++) {
+		for(var x = 0; x<(map.dimensions);x++) {
+			switch(map.layout[x][y]) {
+				case 0:
+					ctx.drawImage(dirt,0,0,tileWidth,tileHeight,y*tileWidth,x*tileHeight,tileWidth,tileHeight)	
+					break;
+				case 1:
+					ctx.drawImage(grass,0,0,tileWidth,tileHeight,y*tileWidth,x*tileHeight,tileWidth,tileHeight)
+					break;
+			}
+		}
+	}
+	
+	return Object.freeze ({
+		drawMap: drawMap,
+	})
+}
--- client/game/entity/level.js
+++ client/game/entity/level.js
@@ -5,14 +5,27 @@ export default function makeLevel() {
 
 	console.log("making levels, ya'll");
 	var level = {};
-	//var graphics = makeLevelGraphicComponent(level);
+	var graphics = makeLevelGraphicComponent({
+		entityOfComponent: level,
+		dirt: 'assets/img/maptiles/dirt-noborder.png';
+		grass: 'assets/img/maptiles/grass-noborder.png';
+	});
 
-	var components = {};
+	var components = {
+		graphics: graphics,
+	};
+
+	level.getMap = function() {
+		return makeLevelMapComponent.map
+	}
 
 	level.getComponentKeys = function() {
 		return Object.keys(components);
 	};
 
+	level.draw = function(ctx,map) {
+		components.graphics.drawMap(ctx,level.getMap())
+	}
 
 	return Object.freeze(level);
 	
