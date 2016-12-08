4e0d3263aa1cade37ad82d53e0c92ed3255071c8
mat <reallymathew@gmail.com>
2016-12-07 20:56:48 -0600

    add input system

--- client/game/game.js
+++ client/game/game.js
@@ -1,4 +1,5 @@
 import graphicsSystem from './system/graphics';
+import inputSystem from './system/input';
 import badguy from './entity/badguy';
 
 export default function game() {
@@ -6,10 +7,12 @@ export default function game() {
 	var entities = [badguy()];
 	var canvas = document.getElementById("canvas");
 	var graphics = graphicsSystem(entities, canvas);
+	var input = inputSystem(entities, canvas);
 
 	function run() {
 		console.log("test");
 		graphics.run();
+		input.run();
 	}
 
 	return Object.freeze ({
