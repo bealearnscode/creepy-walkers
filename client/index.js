da8e5b052c3cf4e51b370994c357702531ab419c
mat <reallymathew@gmail.com>
2016-12-08 19:42:36 -0600

    remove maps directory. Another oops

--- client/index.js
+++ client/index.js
@@ -1,6 +1,4 @@
 import game from './game/game';
-import main from './maps/main';
-//main.js is really just going to be calling the run function from game.js
 
 document.addEventListener("DOMContentLoaded", function() {
 	game().run();
