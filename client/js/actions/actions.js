6c1283fe19d9b800c5d05935b812ff2e9bf1d4f6
Mattressburn <chevyboxer@gmail.com>
2016-12-14 08:11:53 -0500

    trying to figure out reducers and actions

---
+++ client/js/actions/actions.js
@@ -0,0 +1,49 @@
+
+
+var FETCH_GAME = 'FETCH_GAME';
+function fetchGame() {
+	return {
+		type: FETCH_GAME,
+		// payload: no idea what to put here
+	}
+}
+
+var ADD_USER = 'ADD_USER'
+function addUser () {
+	return {
+		// no idea what to put here as well
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+exports.FETCH_GAME = FETCH_GAME;
+exports.fetchGame = fetchGame;
+
+exports.ADD_USER = ADD_USER;
+exports.addUser = addUser;
