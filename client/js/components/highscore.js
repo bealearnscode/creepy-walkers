f106ed5286d247ba8344b61953d7a6538b48185f
mat <reallymathew@gmail.com>
2016-12-15 22:45:03 -0600

    mess with package.json

--- client/js/components/highscore.js
+++ client/js/components/highscore.js
@@ -8,6 +8,10 @@ class HighScore extends Component {
 		this.props.dispatch(actions.fetchHighScores());
 	}
 
+	onHomeClick() {
+		this.props.router.push('/');
+	}
+
 	render() {
 		return(
 			<div>
@@ -15,6 +19,7 @@ class HighScore extends Component {
 				<ul>
 					{this.props.highScores.map((score, index) => <ScoreItem key={index} index={index} score={score} />)}
 				</ul>
+				<button className="btn btn-lg btn-primary btn-block" onClick={this.onHomeClick.bind(this)}>Home</button>
 			</div>
 		);
 	}
--- client/js/components/home.js
+++ client/js/components/home.js
@@ -1,36 +1,69 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import { Router, Route, Link } from 'react-router';
+import * as actions from '../redux/actions';
 
 class Home extends Component {
+	onLogoutClick() {
+		this.props.dispatch(actions.destroySession());
+	}
+
 	render() {
 		let user = this.props.currentUser;
 		let text = "";
 		const signedInText = !user ? (text = "sign in to log your high scores") : (text = "signed in as: " + user);
-		return (
-			<div>
-				<header>
-					<div id="row">
-		                <div className="col-12">
-		                    <div className="intro">
-		                        <h1>Game of Towers</h1>
-		                        <p>{signedInText}</p>
-		                    </div>
-		                </div>
-		            </div>
-				</header>
+		if(!user) {
+			return (
+				<div>
+					<header>
+						<div id="row">
+			                <div className="col-12">
+			                    <div className="intro">
+			                        <h1>Game of Towers</h1>
+			                        <p>{signedInText}</p>
+			                    </div>
+			                </div>
+			            </div>
+					</header>
 
-				<div className="wrapper">
-    				<form className="form-start-options">
-        				<h2 className="form-start-options-heading">Select an Option</h2>
-        				<button className="btn btn-lg btn-primary btn-block"><Link to={`/login`}>Login</Link></button>
-        				<button className="btn btn-lg btn-primary btn-block"><Link to={`/register`}>Register</Link></button>
-        				<button className="btn btn-lg btn-primary btn-block"><Link to={`/game`}>Game</Link></button>
-        				<button className="btn btn-lg btn-primary btn-block"><Link to={`/highscores`}>High Scores</Link></button>
-    				</form>
+					<div className="wrapper">
+	    				<form className="form-start-options">
+	        				<h2 className="form-start-options-heading">Select an Option</h2>
+	        				<button className="btn btn-lg btn-primary btn-block"><Link to={`/login`}>Login</Link></button>
+	        				<button className="btn btn-lg btn-primary btn-block"><Link to={`/register`}>Register</Link></button>
+	        				<button className="btn btn-lg btn-primary btn-block"><Link to={`/game`}>Play Game</Link></button>
+	        				<button className="btn btn-lg btn-primary btn-block"><Link to={`/highscores`}>High Scores</Link></button>
+	    				</form>
+					</div>
 				</div>
-			</div>
-		);
+			);
+		}
+		else {
+			return (
+				<div>
+					<header>
+						<div id="row">
+			                <div className="col-12">
+			                    <div className="intro">
+			                        <h1>Game of Towers</h1>
+			                        <p>{signedInText}</p>
+			                    </div>
+			                </div>
+			            </div>
+					</header>
+
+					<div className="wrapper">
+	    				<form className="form-start-options">
+	        				<h2 className="form-start-options-heading">Select an Option</h2>
+	        				<button className="btn btn-lg btn-primary btn-block"><Link to={`/game`}>Play Game</Link></button>
+	        				<button className="btn btn-lg btn-primary btn-block"><Link to={`/highscores`}>High Scores</Link></button>
+	        				<button className="btn btn-lg btn-primary btn-block" onClick={this.onLogoutClick.bind(this)}>Log Out</button>
+	    				</form>
+					</div>
+				</div>
+			);
+		}
+		
 	}
 }
 
