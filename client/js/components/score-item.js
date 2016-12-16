9192225f1994dbb90e4bb4ff6538d59b880437ef
mat <reallymathew@gmail.com>
2016-12-15 19:16:14 -0600

    remove connect from score-item

--- client/js/components/score-item.js
+++ client/js/components/score-item.js
@@ -1,6 +1,4 @@
 import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import * as actions from '../redux/actions';
 
 export default class ScoreItem extends Component {
 	render() {
