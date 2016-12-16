86fa1d2dab586437102e6704b8d2662ba902dc3f
Mattressburn <chevyboxer@gmail.com>
2016-12-15 22:58:32 -0500

    edit homepage elements's height

--- client/js/components/home.js
+++ client/js/components/home.js
@@ -5,7 +5,7 @@ import { Router, Route, Link } from 'react-router';
 export default class Home extends Component {
 	render() {
 		return (
-			<div>
+			<div className="top-level-component">
 				<header>
 					<div id="row">
 		                <div className="col-12">
